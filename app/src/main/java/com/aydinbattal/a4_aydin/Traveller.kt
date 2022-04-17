99ab0072a839168fecce40fa2f7cd79fd4297d50
Aydin Battal <abattal35@gmail.com>
2022-04-17 19:20:02 -0400

    green - traveller tests (pass)

---
+++ app/src/main/java/com/aydinbattal/a4_aydin/Traveller.kt
@@ -0,0 +1,24 @@
+package com.aydinbattal.a4_aydin
+
+/**
+ * A4-Aydin created by aydin
+ * student ID : 991521740
+ * on 2022-04-17 */
+class Traveller(val name:String, val email:String, val passport:String? = null, val bookings:MutableList<Flight>) {
+    val totalCost: Double
+        get() {
+            var sum = 0.0
+            for (i in bookings){
+                sum += i.cost
+            }
+            return sum
+        }
+
+    fun bookFlight(flight: Flight): Boolean {
+        if (this.passport != null){
+            this.bookings.add(flight)
+            return true
+        }
+        return false
+    }
+}
