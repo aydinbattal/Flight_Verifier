4d36733be9f3b323b20ba3b0721e6a52c95688eb
Aydin Battal <abattal35@gmail.com>
2022-04-17 19:18:07 -0400

    Updated red - traveller tests (fail)

--- app/src/test/java/com/aydinbattal/a4_aydin/TravellerTest.kt
+++ app/src/test/java/com/aydinbattal/a4_aydin/TravellerTest.kt
@@ -12,7 +12,7 @@ class TravellerTest {
     fun `Traveller books a flight with a passport`(){
 //    ● GIVEN: A Traveller with a passport
 //    AND a valid flight
-        val traveller:Traveller = Traveller("Joe", "joe@gmail.com", "CAN123321", listOf<Flight>())
+        val traveller:Traveller = Traveller("Joe", "joe@gmail.com", "CAN123321", mutableListOf<Flight>())
         val flight:Flight = Flight("Pearson Airport", "Vancouver Airport", 1000.0)
 //    ● WHEN: The traveller attempts to book the flight
 //    ● THEN: The flight is booked
@@ -24,7 +24,7 @@ class TravellerTest {
     fun `Traveller books a flight with no passport`() {
 //    ● GIVEN: A Traveller with no passport
 //    AND a valid flight
-        val traveller:Traveller = Traveller("Joe", "joe@gmail.com", null, listOf<Flight>())
+        val traveller:Traveller = Traveller("Joe", "joe@gmail.com", null, mutableListOf<Flight>())
         val flight:Flight = Flight("Pearson Airport", "Vancouver Airport", 1000.0)
 //    ● WHEN: The traveller attempts to book the flight
 //    ● THEN: The flight is is not booked
@@ -35,16 +35,16 @@ class TravellerTest {
     @Test
     fun `Traveller has no bookings and wants the price of their reservations`() {
 //        ● GIVEN: A Traveller with no bookings
-        val traveller:Traveller = Traveller("Joe", "joe@gmail.com", "CAN123321", listOf<Flight>())
+        val traveller:Traveller = Traveller("Joe", "joe@gmail.com", "CAN123321", mutableListOf<Flight>())
 //        ● WHEN: The ttraveller checks the amount owed for their flights
 //        ● THEN: The cost is 0
-        assertThat(traveller.totalCost == 0).isTrue()
+        assertThat(traveller.totalCost == 0.0).isTrue()
     }
 
     @Test
     fun `Traveller has one bookings and wants the price of their reservations`() {
 //    ● GIVEN: A Traveller has 1 booking
-        val traveller:Traveller = Traveller("Joe", "joe@gmail.com", "CAN123321", listOf<Flight>(
+        val traveller:Traveller = Traveller("Joe", "joe@gmail.com", "CAN123321", mutableListOf<Flight>(
             Flight("Pearson Airport", "Vancouver Airport", 1000.0)))
 //    ● WHEN: The ttraveller checks the amount owed for their flights
 //    ● THEN: The cost is is the price of the single flight
@@ -56,7 +56,7 @@ class TravellerTest {
     @Test
     fun `Traveller has multiple bookings and wants the price of their reservations`() {
 //    ● GIVEN: A Traveller has more than 1 booking (example, 3)
-        val traveller:Traveller = Traveller("Joe", "joe@gmail.com", "CAN123321", listOf<Flight>(
+        val traveller:Traveller = Traveller("Joe", "joe@gmail.com", "CAN123321", mutableListOf<Flight>(
             Flight("Pearson Airport", "Vancouver Airport", 1000.0),
             Flight("Pearson Airport", "Vancouver Airport", 1100.5),
             Flight("Pearson Airport", "Vancouver Airport", 900.0)))
